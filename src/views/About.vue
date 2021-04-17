662f05ab4ecf7a3b3f8e2b21233d36aa0653a44d
hgaogao <hgaogaogao@gmail.com>
2021-04-17 23:33:46 +0800

    created

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <van-button plain type="primary">朴素按钮</van-button>
+    <van-button plain type="primary">朴素按钮</van-button>
   </div>
 </template>
